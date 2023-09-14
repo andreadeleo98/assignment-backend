e3ea17ae290c04594ccfc6274ccd0dbda8ec11d1
andreadeleo <andreadeleo1998@gmail.com>
2023-09-14 12:01:33 +0200

    file non utilizzati

--- src/main/java/com/example/demo/repository/DdiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.model.Ddi; // Import the correct Ddi class
-
-public interface DdiRepository extends MongoRepository<Ddi, String> {
-    // Puoi aggiungere metodi personalizzati per query specifiche, se necessario
-}
